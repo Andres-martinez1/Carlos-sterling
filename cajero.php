e6ba31f1774aba9180b1ce50d85ce66aa54af264
andres-martinez1 <andresfelipepenamartinez99@gmail.com>
2024-06-25 10:39:16 -0500

    Realizo el menú de interacción con el usuario y la consola

---
+++ cajero.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "BIENVENIDO AL CAJERO AUTOMATICO EL DESCONTROL"."\n",
+"{1} Ingresar dinero."."\n",
+"{2} Consultar saldo."."\n",
+"{3} Consultar dinero."."\n",
+"{4} Retirar dinero."."\n";
+
+
+$opcion = readline("Ingrese el numero de la opcion que desea realizar:");
+
+if ($opcion == "1"){
+    echo "¿Cuanto dinero desea ingresar?";
+    }
+if ($opcion == "2"){
+echo "Tu saldo hasta el momento es:";
+}
+if ($opcion == "3"){
+echo "Tu dinero hasta el momento es:";
+}
+if ($opcion == "4"){
+echo "¿Cuanto dinero deseas retirar?:";
+}
+
